359520d51e2cd6b83a89f77a8f54484764a46d4b
Murigi-Nganga <graciousngangamurigi@gmail.com>
2022-04-19 23:36:33 +0300

    add resources and assets

--- app/src/main/java/com/example/agrix/MainActivity.kt
+++ app/src/main/java/com/example/agrix/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.agrix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.agrix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Binding object
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Using View Binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
